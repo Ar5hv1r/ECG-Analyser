9c5f68de9cfe49c581b731a9cbad4e29a56b22de
Ar5hv1r <arshvirt2@gmail.com>
2024-02-20 23:14:13 +0000

    FEAT: initial pytorch implementation for making predictions on new data

---
+++ predict.py
@@ -0,0 +1,34 @@
+import torch
+import numpy as np
+import argparse
+from datasets import ECGDataset
+from model import get_model
+from torch.utils.data import DataLoader
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get performance on test set from hdf5")
+    parser.add_argument('path_to_hdf5', type=str, help='path to hdf5 file containing tracings')
+    parser.add_argument('path_to_model', help='path to the trained PyTorch model file')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for testing')
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cude.is_available() else "cpu")
+
+    test_dataset = ECGDataset(args.path_to_hdf5, None)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
+    model = get_model(n_classes=6) #adjustable
+    model.load_state_dict(torch.load(args.path_to_model, map_location=device))
+    model.to(device)
+    model.eval()
+
+    predictions = []
+    with torch.no_grad():
+        for inputs in test_loader:
+            inputs = inputs.to(device)
+            outputs = model(inputs)
+            predictions.append(outputs.sigmoid().cpu().numpy()) # can add sigmoid function
+
+    predictions = np.concatenate(predictions, axis=0)
+    np.save(args.output_file, predictions)
+    print("Output predictions saved")
